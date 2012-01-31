4fd6ceac7e8efca9136a3b5a87dd793f6a076439
Joseph Mornin <joseph@mornin.org>
2012-01-30 20:15:51 -0500

    Add sidebar content

--- sidebar.php
+++ sidebar.php
@@ -15,19 +15,25 @@ if ( 'content' != $current_layout ) :
 		<div id="secondary" class="widget-area" role="complementary">
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
+                                <aside id="about" class="widget">
+                                    <h3>About</h3>
+                                    <p>The Hewlett Grantees Meeting is a lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin blandit dictum eleifend. <a href="">More...</a></p>
+                                </aside>
+
+                                <aside id="about" class="widget">
+                                    <h3>Participants</h3>
+                                    [thumbnails of participant photos?]
+                                </aside>
+
+                                <aside id="about" class="widget">
+                                    <h3>Tweets</h3>
+                                    [...]
+                                </aside>
+
 				<aside id="archives" class="widget">
-					<h3 class="widget-title"><?php _e( 'Archives', 'twentyeleven' ); ?></h3>
+					<h3 class="widget-title"><?php _e( 'Announcements', 'twentyeleven' ); ?></h3>
 					<ul>
 						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
-					</ul>
-				</aside>
-
-				<aside id="meta" class="widget">
-					<h3 class="widget-title"><?php _e( 'Meta', 'twentyeleven' ); ?></h3>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
 					</ul>
 				</aside>
 
